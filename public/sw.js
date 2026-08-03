e5b83cd3144a09686045a5738cd5446cee7a1e93
compta-plus-debug <firetoriel08@gmail.com>
2026-08-03 09:40:34 -0600

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,36 @@
+const CACHE_NAME = "compta-plus-cache-v1";
+const APP_SHELL = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+];
 
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  if (new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
